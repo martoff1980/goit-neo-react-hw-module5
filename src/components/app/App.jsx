6b58642f104cfea2eb1b2415da59e41ed25631f0
martoff <martoff@ukr.net>
2025-05-06 09:22:49 +0300

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -22,7 +22,10 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+          <Route path="/movies/:movieId/" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
           <Route path="/404" element={<NotFoundPage />} />
           <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
